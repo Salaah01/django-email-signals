ab68431f29d814d5c392efa274b33fb62e677feb
Salaah Amin <Salaah01@users.noreply.github.com>
2022-03-16 15:59:57 +0000

    removed some npm packages.

--- webpack.common.js
+++ webpack.common.js
@@ -31,22 +31,6 @@ module.exports = {
         exclude: /\.module\.(scss|css)$/,
       },
       {
-        test: /\.svg$/,
-        use: "file-loader",
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
-      },
-      {
         test: /\.(ts|tsx)$/,
         use: "ts-loader",
         exclude: /node_modules/,
